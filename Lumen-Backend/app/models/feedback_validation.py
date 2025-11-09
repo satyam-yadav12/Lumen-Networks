5037ef404b1289f78054d96a586d7e91abb02253
satyam-yadav12 <satyam.dev637@gmail.com>
2025-11-10 00:04:15 +0530

    renamed folder Models to models

---
+++ Lumen-Backend/app/models/feedback_validation.py
@@ -0,0 +1,14 @@
+from marshmallow import fields, Schema
+
+
+class feedback_validation(Schema):
+    Email = fields.Email(
+        required=True, error_messages={"invalid Email": "Enter a valid Email"}
+    )
+    Name = fields.String(required=True, error_messages={"required": "name is required"})
+    Title = fields.String(
+        required=True, error_messages={"required": "message title is required"}
+    )
+    Description = fields.String(
+        required=True, error_messages={"required": "Description field cannot be empty"}
+    )
